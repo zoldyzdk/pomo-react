a710afa1211e4910e0f6782c2cd2726a905bdee9
Hewerton <hewertonlourenco@gmail.com>
2023-03-17 20:22:42 -0300

    Feat: adding theme select

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import TwentyFiveMinTimer from "./TwentyFiveMinTimer";
 const NavBar = () => {
   return (
       <div className="navbar bg-base-100">
-        <div className="flex-1">
+        <div className=" flex-1">
           <a className="btn btn-ghost normal-case text-xl" href="https://github.com/Sherlockzen/pomo-react" target="_blank">
             PomoReact
           <img className=" w-12" src={github} alt="Github Icon" />
@@ -15,37 +15,39 @@ const NavBar = () => {
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li><a>Config</a></li>
-
           </ul>
         </div>
       </div>
   )
 }
 
-// function Counter() {
-//     const [count, setCount] = useState(0);
-//
-//     useEffect(() => {
-//         const intervalId = setInterval(() => {
-//             setCount(c => c + 1); // ✅ Pass a state updater
-//         }, 1000);
-//         return () => clearInterval(intervalId);
-//     }, []); // ✅ Now count is not a dependency
-//
-//     return <h1>{count}</h1>;
-// }
+const Themes = ({ OnChange }) => {
+  return (
+    <select
+      onChange={(e) => OnChange(e.target.value)}
+      className="select select-bordered w-full max-w-xs"
+    >
+      <option disabled selected>
+        Select the Theme
+      </option>
+      <option>light</option>
+      <option>dark</option>
+      <option>cupcake</option>
+      <option>bumblebee</option>
+      <option>emerald</option>
+      <option>corporate</option>
+      <option>synthwave</option>
+      <option>retro</option>
+      <option>cyberpunk</option>
+      <option>valentine</option>
+      <option>halloween</option>
+      <option>forest</option>
+    </select>
+  );
+};
 
 const Timer = () => {
   const [count, setCount] = useState(0)
-      // useEffect(() => {
-      //   const intervalId = setInterval(() => {
-      //     setCount((c) => c + 1);
-      //   }, 1000);
-      //   return () => clearInterval(intervalId);
-      // }, []);
-
-    function handleStart() {
-    }
 
   return (
     <div className=" flex flex-col p-10 gap-10 items-center">
@@ -63,22 +65,21 @@ const Timer = () => {
 }
 
 function App() {
-
-
+const [theme, setTheme] = useState('dark');
 
   return (
-    <div data-theme="light" className="App h-[100svh] grid justify-items-center">
-      <div className=" max-w-3xl">
+    <div data-theme={theme} className="App h-[100svh] grid justify-items-center">
+      <div className=" max-w-3xl flex flex-col items-center">
         <NavBar />
         <div className=" p-2 sm:p-10 text-center flex flex-col items-center justify-center">
-          <div className="navbar bg-base-100 text-base flex flex-wrap justify-center">
+          <div className="navbar bg-base-100 text-base w-full flex flex-wrap justify-center">
             <button className="btn btn-ghost btn-active btn-sm normal-case h-1 p-1">Pomodoro</button>
             <a className="btn btn-ghost normal-case btn-sm p-1">Intervalo Curto</a>
             <a className="btn btn-ghost normal-case btn-sm p-1">Intervalo Longo</a>
           </div>
-
         </div>
         <TwentyFiveMinTimer />
+        <Themes OnChange={(theme) => setTheme(theme)}/>
       </div>
     </div>
   );
